409910e9deec495117b946ce1093fff8504a4402
Sukanyastudy <139069301+Sukanyastudy@users.noreply.github.com>
2023-12-13 23:08:07 +0530

    Update GA8.py

--- GA8.py
+++ GA8.py
@@ -8,13 +8,11 @@ This app calculates the largest of 3 numbers
 #Get Input
 
 st.header('User Input Parameters')
-
-def user_input_features():
-    num1 = st.number_input("First Number")
-    num2 = st.number_input("Second Number")
-    num3 = st.number_input("Third Number")
-    return max(num1,num2,num3)
+num1 = st.number_input("First Number")
+num2 = st.number_input("Second Number")
+num3 = st.number_input("Third Number")
 
 if st.button("Find the Largest Number of 3"):
-      largest_number = user_input_features()
+    
+      largest_number = max(num1,num2,num3)
       st.success(f"The largest number is: {largest_number}")
